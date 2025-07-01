01089da7ae1c63b7ca23cb93ae25697eb3b1ca23
Prstn <ooishehpang@gmail.com>
2025-07-02 07:39:49 +0800

    fetch the api data of the given username's github account

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import argparse
+import requests
+import json
 
 parser = argparse.ArgumentParser()  # instantiate the ArgumentParser class
 parser.add_argument("username")     # specify a positional argument
 args = parser.parse_args()          # run the parser, extracts data into args (an argparse.Namespace object)
 print(args.username)
 
+base_url = "https://api.github.com/users"
+url = f"{base_url}/{args.username}/events"  # using f-string to embed username inside the url
+print(url)
+
+r = requests.get(url)
+
+print(r.text)
