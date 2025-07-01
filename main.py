75a688af061d4be5ba8ceb9cce58f37bc0320c83
Prstn <ooishehpang@gmail.com>
2025-07-02 07:23:53 +0800

    implement argument parsing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 
 parser = argparse.ArgumentParser()  # instantiate the ArgumentParser class
-parser.add_argument("username")     # define the expected argument
-args = parser.parse_args()
+parser.add_argument("username")     # specify a positional argument
+args = parser.parse_args()          # run the parser, extracts data into args (an argparse.Namespace object)
 print(args.username)
 
