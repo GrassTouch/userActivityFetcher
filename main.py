f6ceca215aac004cf0eb6c2545e9cb7e2ea6779d
GrassTouch <134407126+GrassTouch@users.noreply.github.com>
2025-08-05 14:27:45 +0800

    changed the input method from cli argument to user prompt

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ import argparse
 import requests
 import json
 
-parser = argparse.ArgumentParser()  # instantiate the ArgumentParser class
-parser.add_argument("username")     # specify a positional argument
-args = parser.parse_args()          # run the parser, extracts data into args (an argparse.Namespace object)
-print(args.username)
+# cli argument method
+# parser = argparse.ArgumentParser()  # instantiate the ArgumentParser class
+# parser.add_argument("username")     # specify a positional argument
+# args = parser.parse_args()          # run the parser, extracts data into args (an argparse.Namespace object)
+# print(args.username)
+
+# prompt for input method
+username = input("enter a github username: ")
+
 
 base_url = "https://api.github.com/users"
-url = f"{base_url}/{args.username}/events"  # using f-string to embed username inside the url
+url = f"{base_url}/{username}/events"  # using f-string to embed username inside the url
 print(url)
 
 r = requests.get(url)
-
-print(r.text)
+dict = r.json()
+print(dict)
+# print(r.text)
+# print(r.content)
